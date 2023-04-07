1910086df4beb70788b907cff1aacb021662cc17
Vinicius José <viniciusjosedev@gmail.com>
2023-04-07 15:23:36 -0300

    create: requirement 3 done

--- challenges/desafio3.sql
+++ challenges/desafio3.sql
@@ -1,9 +1,5 @@
-SELECT * FROM SpotifyClone.usuarios;
-SELECT * FROM SpotifyClone.historico;
-SELECT * FROM SpotifyClone.cancoes;
-
 SELECT u.nome_pessoa_usuaria AS pessoa_usuaria, COUNT(h.pessoa_usuaria_id) AS musicas_ouvidas,
-AVG(c.duracao_segundos) AS total_minutos
+ROUND(SUM(c.duracao_segundos)/60, 2) AS total_minutos
 FROM SpotifyClone.usuarios u
 INNER JOIN SpotifyClone.historico h
 ON u.pessoa_usuaria_id = h.pessoa_usuaria_id
